7fd6af287b46862ddb46c0900b5d09884c4e7be8
wiyadathi <wiyada.t@bu.ac.th>
2026-02-25 15:13:07 +0700

    Raycast Push Up

--- Assets/Script/BoxPushUp.cs
+++ Assets/Script/BoxPushUp.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class BoxPushUp : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Rigidbody rb;
+    [SerializeField] Rigidbody rotateObjRB;
+    [SerializeField] GameObject rotateObj;
+
+    [SerializeField] float force;
+    [SerializeField] float torque;
+    [SerializeField] float rayDistance = 3.0f;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rotateObjRB = rotateObj.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Debug.DrawRay(transform.position, Vector3.down * rayDistance, Color.red);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, rayDistance))
+        {
+            if (hit.collider.name == "Capsule")
+            {
+                Debug.Log("Hit: " + hit.collider.name);
+                Debug.DrawRay(transform.position, Vector3.down * rayDistance, Color.blue);
+
+                if (rb != null) //คำสั่งนี้ไม่มีก็ได้ ไม่หักคะแนน
+                {
+                    rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+                    rotateObjRB.isKinematic = true;
+                }
+
+            }
+        }
     }
 }
