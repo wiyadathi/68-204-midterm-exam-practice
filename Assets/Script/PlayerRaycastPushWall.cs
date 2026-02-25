47dc567e5b75a66137597702d2bcfe1227265e0e
wiyadathi <wiyada.t@bu.ac.th>
2026-02-25 15:00:16 +0700

    s3 scene

--- Assets/Script/PlayerRaycastPushWall.cs
+++ Assets/Script/PlayerRaycastPushWall.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerRaycastRotate : MonoBehaviour
+public class PlayerRaycastPushWall : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
     [SerializeField] Rigidbody wallRB;
